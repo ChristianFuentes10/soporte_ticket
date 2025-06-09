8b7b1dd95d50119fc487b5306d3caae95667c14d
Skibidi-Mate <mate.hernandezm@duocuc.cl>
2025-06-09 18:13:29 -0400

    se cambio la ruta

--- src/main/java/com/api_soporte_ticket/controller/SoporteTicketController.java
+++ src/main/java/com/api_soporte_ticket/controller/SoporteTicketController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/soporte/tickets")
+@RequestMapping("/api/soporte/tickets")
 public class SoporteTicketController {
 
     @Autowired
